fbc743ef9bc0e212e17ed8e0d30b05abec7c8ed2
uhyo <uhyo@uhyohyo.net>
2015-10-03 20:46:42 +0900

    update inputrecorder

--- site/masao/InputRecorder.js
+++ site/masao/InputRecorder.js
@@ -1,27 +1,6 @@
 // Input Recorder
 //
 
-/*
- * Masao Playlog Format: METADATA (HEADER BODY)*
- *
- * METADATA block:
- *   magic: 0x4D 0x0E 0x50 0x0A
- *   version 4 octets | MPF version in Big-Endian (0x00 0x00 0x00 0x01)
- *   reserved: 0x00 0x00 0x00 0x0C
- *
- * HEADER block:
- *   seed: 4 octets | initial ran_seed of succeeding BODY block in Big-Endian
- *   stage: 1 octet | stage number
- *   status:1 octet | 0x00 = aborted/miss, 0x01 = cleared
- *   reserved: 0x00 0x00
- *   score: 4 octets| eventual score of this log
- *   length: 4 octets | length in bytes of succeeding BODY block in Big-Endian
- *
- * BODY block:
- *   fxxxxxxx | f: 0 = release, 1 = press. xxxxxxx: interval in frames
- *   kkkkkkkk | k: key code. (0 = nop)
- */
-
 CanvasMasao.InputRecorder = (function(){
     var InputRecorder = function(mc, inputdataCallback){
         this.mc=mc;
@@ -37,6 +16,13 @@ CanvasMasao.InputRecorder = (function(){
         this.ran_seed=null; //最初のran_seed
         this.stage=null;    //現在のステージ
         this.recording = false;
+        this.tr1_key = 0;   //TR1に該当するキー（Space or Z）
+        //最後のコンティニュー情報
+        this.prev_cpoint_con = 0;
+        this.prev_cpoint_stage = 0;
+        this.prev_cpoint_x = 32;
+        this.prev_cpoint_y = 320;
+        this.continued = false; //プレイごとに管理
         //バッファ
         this.allbuf=[]; //全ステージ通しのデータが入ったバッファ
         this.buffers=[];
@@ -61,6 +47,8 @@ CanvasMasao.InputRecorder = (function(){
                 _this.save(paramKeyEvent.keyCode, false);
             }
         };
+
+        this.reset();
     };
     InputRecorder.prototype.initBuffer = function(){
         //バッファを作成
@@ -71,10 +59,10 @@ CanvasMasao.InputRecorder = (function(){
         //入力情報をバッファに保存
         var frame=this.frame, last_frame=this.last_frame;
         var sa=frame-last_frame;
-        //前の入力との差は127フレームまでしか記録できないので、それを超える場合はnopをはさむ
-        while(sa>127){
-            this.pushData(false, 127, 0);
-            sa-=127;
+        //前の入力との差は15フレームまでしか記録できないので、それを超える場合はnopをはさむ
+        while(sa>15){
+            this.pushData(false, 15, 0);
+            sa-=15;
         }
         //キーデータ
         this.pushData(pressed, sa, keyCode);
@@ -91,18 +79,37 @@ CanvasMasao.InputRecorder = (function(){
         }
     };
     InputRecorder.prototype.pushData = function(pressed_flag, interval, keyCode){
+        //3bitのキーコード
+        var k1 = this.keyBits(keyCode);
+        if(k1!==7){
+            //1 octetであらわす
+            this.addToBuffer(((+pressed_flag)<<7) | ((interval & 15)<<3) | (k1&7));
+        }else{
+            //2 octetsであらわす
+            this.addToBuffer(((+pressed_flag)<<7) | ((interval & 15)<<3) | 7);
+            this.addToBuffer(keyCode);
+        }
+    };
+    InputRecorder.prototype.addToBuffer = function(char){
+        //実際にbufferをいじる
         if(this.current_buffer_size===this.current_buffer.length){
             //満杯なので次のバッファを作る
             this.buffers.push(this.current_buffer);
             this.initBuffer();
         }
-        var sz=this.current_buffer_size, buf=this.current_buffer;
-        buf[sz] = ((+pressed_flag)<<7) | (interval & 0x7F);
-        buf[sz+1] = keyCode;
-        this.current_buffer_size=sz+2;
+        this.current_buffer[this.current_buffer_size++] = char;
+    };
+    InputRecorder.prototype.reset = function(){
+        //タイトル画面時の初期化
+        this.allbuf=[];
+        this.buffers=[];
+        this.prev_cpoint_con = 0;
+        this.prev_cpoint_stage = 0;
+        this.prev_cpoint_x = 0;
+        this.prev_cpoint_y = 0;
     };
     InputRecorder.prototype.masaoEvent = function(g,image){
-        var mc=this.mc, ml_mode=mc.mp.ml_mode;
+        var mc=this.mc, mp=mc.mp, ml_mode=mp.ml_mode;
         var prev_playing = this.prev_ml_mode===100 || this.prev_ml_mode===110,
             playing = ml_mode===100 || ml_mode===110;
         if(!prev_playing && playing){
@@ -110,9 +117,16 @@ CanvasMasao.InputRecorder = (function(){
             this.initBuffer();
             this.frame=0;
             this.last_frame=0;
-            this.stage = mc.mp.stage;
-            this.ran_seed = mc.mp.ran_seed;
+            this.stage=mp.stage;
+            this.tr1_key=0;
+            this.ran_seed=mp.ran_seed;
             this.recording=true;
+            //コンティニュー関係の初期化
+            this.continued=false;
+            this.prev_cpoint_con = mp.cpoint_con;
+            this.prev_cpoint_stage = mp.cpoint_stage;
+            this.prev_cpoint_x = mp.cpoint_x;
+            this.prev_cpoint_y = mp.cpoint_y;
 
             //0フレーム目の入力を記録
             this.saveCurrentPressing();
@@ -128,7 +142,7 @@ CanvasMasao.InputRecorder = (function(){
             });
             //種類(クリアした="clear", やられた="miss", 中断した="abort")
             var status;
-            if(mc.mp.stage !== this.stage || ml_mode===260 || ml_mode >= 400){
+            if(mp.stage !== this.stage || ml_mode===260 || ml_mode >= 400){
                 //ステージが進行した or ゲームクリア画面なのでステージクリアした
                 status="clear";
             }else if(ml_mode>=300 || ml_mode===90 || ml_mode===250){
@@ -138,7 +152,7 @@ CanvasMasao.InputRecorder = (function(){
                 status="abort";
             }
             //最終スコア
-            var score=mc.mp.score;
+            var score=mp.score;
 
             //HEADER blockを作成
             var header = new Uint8Array(16);
@@ -148,7 +162,11 @@ CanvasMasao.InputRecorder = (function(){
             //stage
             header_view.setUint8(4, this.stage);
             //status
-            header_view.setUint8(5, status==="clear" ? 1 : 0);
+            /// 1bit目（clearかどうか）
+            var status_octet = status==="clear" ? 1 : 0;
+            /// 2bit目（TR1がZかどうか）
+            status_octet |= this.tr1_key===90 ? 2 : 0;
+            header_view.setUint8(5, status_octet);
             //reserved
             header_view.setUint16(6,0);
             //score
@@ -158,9 +176,14 @@ CanvasMasao.InputRecorder = (function(){
             //HEADER blockを結合
             result_buffers = [header].concat(result_buffers);
 
+            //クリアデータに必要か？
+            var required = status==="clear" || this.continued;
+
             //状況を表すオブジェクトを作る
             var result={
                 status: status,
+                continued: this.continued,
+                required: required,
                 //入力データが表すステージ(1-4)
                 stage: this.stage,
                 //最終スコア
@@ -171,14 +194,14 @@ CanvasMasao.InputRecorder = (function(){
                 this.inputdataCallback(result);
             }
             //記録
-            if(status==="clear"){
+            if(required){
                 //クリアのログを残す
                 this.allbuf = this.allbuf.concat(result_buffers);
                 if(ml_mode>=400 && this.inputdataCallback != null){
                     //全部終わったので結果をアレする
                     //METADATA blockを作る
                     var metadata = new Uint8Array([0x4D, 0x0E, 0x50, 0x0A,
-                                                  0x00, 0x00, 0x00, 0x01,
+                                                  0x00, 0x00, 0x00, 0x02,
                                                   0x00, 0x00, 0x00, 0x0C
                     ]);
                     this.allbuf = [metadata].concat(this.allbuf);
@@ -194,12 +217,19 @@ CanvasMasao.InputRecorder = (function(){
             }
             if(ml_mode===50){
                 //タイトルに戻った
-                this.allbuf = [];
-                this.buffers= [];
+                this.reset();
             }
             this.recording=false;
-        }else{
+        }else if(playing){
             this.frame++;
+            if(this.prev_cpoint_con !== mp.cpoint_con || this.prev_cpoint_stage !== mp.cpoint_stage || this.prev_cpoint_x !== mp.cpoint_x || this.prev_cpoint_y != mp.cpoint_y){
+                //コンティニューされた
+                this.continued=true;
+                this.prev_cpoint_con = mp.cpoint_con;
+                this.prev_cpoint_stage = mp.cpoint_stage;
+                this.prev_cpoint_x = mp.cpoint_x;
+                this.prev_cpoint_y = mp.cpoint_y;
+            }
         }
         this.prev_ml_mode = ml_mode;
     };
@@ -224,6 +254,46 @@ CanvasMasao.InputRecorder = (function(){
             sum+=size;
         }
         return result;
+    };
+    InputRecorder.prototype.keyBits = function(keyCode){
+        //keyCodeをアレにする
+        if(keyCode===0){
+            //nop
+            return 0;
+        }else if(keyCode===37 || keyCode===100){
+            //LEFT
+            return 1;
+        }else if(keyCode===38 || keyCode===104){
+            //UP
+            return 2;
+        }else if(keyCode===39 || keyCode===102){
+            //RIGHT
+            return 3;
+        }else if(keyCode===40 || keyCode===98){
+            //DOWN
+            return 4;
+        }else if(keyCode===88){
+            //X
+            return 6;
+        }else if(this.tr1_key===keyCode){
+            //TR1
+            return 5;
+        }else{
+            if(this.tr1_key===0){
+                //まだTR1が定まっていない
+                if(keyCode===90){
+                    //Z
+                    this.tr1_key=90;
+                    return 5;
+                }else if(keyCode===32){
+                    //Space
+                    this.tr1_key=32;
+                    return 5;
+                }
+            }
+            //その他のキーだ
+            return 7;
+        }
     };
     InputRecorder.inject = function(mc, options){
         var _ui=mc.userInit, _us=mc.userSub;
